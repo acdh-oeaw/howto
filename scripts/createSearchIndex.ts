4186724d68b62155381e1976ac5afe5a16a5e3cd
Stefan Probst <stefanprobst@kolabnow.com>
2021-07-19 11:45:33 +0200

    build: initialize search index

--- scripts/createSearchIndex.ts
+++ scripts/createSearchIndex.ts
@@ -1,27 +1,33 @@
+import { loadEnvConfig } from '@next/env'
 import algoliasearch from 'algoliasearch'
 import type { SearchIndex } from 'algoliasearch'
 
 import { getPostPreviews } from '@/cms/api/posts.api'
 import type { IndexedResource } from '@/cms/api/resources.api'
 import { log } from '@/utils/log'
+import { noop } from '@/utils/noop'
+
+loadEnvConfig(process.cwd(), false, { info: noop, error: log.error })
 
 /**
- * Returns algolia search client configured with write permissions.
+ * Returns algolia search client configured with admin permissions.
  */
 function getAlgoliaSearchIndex(): SearchIndex | null {
   if (
     process.env.NEXT_PUBLIC_ALGOLIA_APP_ID == null ||
-    process.env.ALGOLIA_API_KEY == null ||
+    process.env.ALGOLIA_ADMIN_API_KEY == null ||
     process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME == null
   ) {
-    throw new Error(
+    const error = new Error(
       'Failed to update search index because no Algolia config was provided.',
     )
+    delete error.stack
+    throw error
   }
 
   const searchClient = algoliasearch(
     process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-    process.env.ALGOLIA_API_KEY,
+    process.env.ALGOLIA_ADMIN_API_KEY,
   )
 
   const searchIndex = searchClient.initIndex(
