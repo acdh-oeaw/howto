5f8e42a0dfb99003909be8b153ed719cfefa5963
Stefan Probst <stefanprobst@kolabnow.com>
2021-07-20 18:24:11 +0200

    fix: provide translations to preview

--- src/cms/previews/Preview.tsx
+++ src/cms/previews/Preview.tsx
@@ -1,9 +1,12 @@
 import ErrorBoundary, { useError } from '@stefanprobst/next-error-boundary'
 import type { PreviewTemplateComponentProps } from 'netlify-cms-core'
 import type { ReactNode } from 'react'
+import { useState, useEffect } from 'react'
 
 import { PreviewProvider } from '@/cms/previews/Preview.context'
 import { I18nProvider } from '@/i18n/I18n.context'
+import type { Dictionary } from '@/i18n/loadDictionary'
+import { loadDictionary } from '@/i18n/loadDictionary'
 
 export interface PreviewProps extends PreviewTemplateComponentProps {
   children?: ReactNode
@@ -14,11 +17,20 @@ export interface PreviewProps extends PreviewTemplateComponentProps {
  */
 export function Preview(props: PreviewProps): JSX.Element {
   const locale = props.entry.getIn(['data', 'lang'], 'en')
+  const [dictionary, setDictionary] = useState<
+    { [namespace: string]: Dictionary } | undefined
+  >(undefined)
+
+  useEffect(() => {
+    loadDictionary(locale, ['common']).then((dictionary) =>
+      setDictionary(dictionary),
+    )
+  }, [locale])
 
   return (
     <PreviewProvider {...props}>
       <ErrorBoundary fallback={ErrorFallback}>
-        <I18nProvider locale={locale} dictionary={undefined}>
+        <I18nProvider locale={locale} dictionary={dictionary}>
           <div className="flex flex-col p-8">{props.children}</div>
         </I18nProvider>
       </ErrorBoundary>
