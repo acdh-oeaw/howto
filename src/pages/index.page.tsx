fc22b2f40978f95e33fbb540c0e75866912846ed
Stefan Probst <stefanprobst@kolabnow.com>
2021-07-25 18:44:38 +0200

    feat: add hero button

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,5 @@
 import type { GetStaticPropsContext, GetStaticPropsResult } from 'next'
+import Link from 'next/link'
 import { Fragment } from 'react'
 
 import { PageContent } from '@/common/PageContent'
@@ -9,6 +10,7 @@ import { useI18n } from '@/i18n/useI18n'
 import { Metadata } from '@/metadata/Metadata'
 import { useAlternateUrls } from '@/metadata/useAlternateUrls'
 import { useCanonicalUrl } from '@/metadata/useCanonicalUrl'
+import { routes } from '@/navigation/routes.config'
 import Mdx, { metadata } from '~/content/pages/home.mdx'
 
 export interface HomePageMetadata extends Record<string, unknown> {
@@ -55,8 +57,8 @@ export default function HomePage(_props: HomePageProps): JSX.Element {
         languageAlternates={languageAlternates}
       />
       <PageContent className="text-neutral-100 bg-gradient-to-br from-neutral-800 to-neutral-900">
-        <div className="grid items-center w-full h-full p-8 mx-auto max-w-80ch">
-          <div className="flex flex-col-reverse">
+        <div className="flex flex-col p-8 mx-auto max-w-80ch">
+          <div className="flex flex-col-reverse py-48">
             <h1
               className="text-6xl font-black tracking-tighter xs:text-7xl md:text-8xl"
               style={{ lineHeight: 0.8 }}
@@ -67,7 +69,14 @@ export default function HomePage(_props: HomePageProps): JSX.Element {
               {subtitle}
             </h2>
           </div>
-          <div>
+          <div className="py-16">
+            <Link href={routes.resources({ kind: 'posts' })}>
+              <a className="inline-flex px-12 py-4 text-lg font-medium tracking-wide text-white uppercase transition rounded bg-primary-600 hover:bg-primary-700 focus:outline-none focus-visible:ring focus-visible:ring-primary-600">
+                {t('common.browse')}
+              </a>
+            </Link>
+          </div>
+          <div className="flex-1">
             <Mdx />
           </div>
         </div>
