fa3faf4835f8b82de1bee28348325efb118f456e
Stefan Probst <stefanprobst@posteo.de>
2021-08-26 17:56:58 +0200

    fix: avoid sending blurdata with metadata

--- scripts/dumpMetadata.ts
+++ scripts/dumpMetadata.ts
@@ -16,12 +16,31 @@ async function main() {
 
   const resourcesPreviews = await getPostPreviews(locale)
   const resources = resourcesPreviews.map((preview) => {
+    if (typeof preview.featuredImage === 'object') {
+      delete preview.featuredImage.blurDataURL
+    }
     return {
       ...preview,
       authors: preview.authors.map((author) => {
-        if (typeof author.avatar === 'object') delete author.avatar.blurDataURL
+        if (typeof author.avatar === 'object') {
+          delete author.avatar.blurDataURL
+        }
         return author
       }),
+      editors:
+        preview.editors?.map((editor) => {
+          if (typeof editor.avatar === 'object') {
+            delete editor.avatar.blurDataURL
+          }
+          return editor
+        }) ?? [],
+      contributors:
+        preview.contributors?.map((contributor) => {
+          if (typeof contributor.avatar === 'object') {
+            delete contributor.avatar.blurDataURL
+          }
+          return contributor
+        }) ?? [],
     }
   })
   fs.writeFileSync(
@@ -34,12 +53,16 @@ async function main() {
 
   const curriculaPreviews = await getCoursePreviews(locale)
   const curricula = curriculaPreviews.map((preview) => {
+    if (typeof preview.featuredImage === 'object') {
+      delete preview.featuredImage.blurDataURL
+    }
     return {
       ...preview,
       editors:
         preview.editors?.map((author) => {
-          if (typeof author.avatar === 'object')
+          if (typeof author.avatar === 'object') {
             delete author.avatar.blurDataURL
+          }
           return author
         }) ?? [],
     }
