ac5189447eadc75b72c7e8dcb6977f352709522a
Stefan Probst <stefanprobst@posteo.de>
2022-03-31 10:56:50 +0200

    content: update home page text

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -43,7 +43,7 @@ export async function getStaticProps(
  * Home page.
  */
 export default function HomePage(_props: HomePageProps): JSX.Element {
-  const { title, subtitle } = metadata as HomePageMetadata
+  const { title } = metadata as HomePageMetadata
 
   const { t } = useI18n()
   const canonicalUrl = useCanonicalUrl()
@@ -57,27 +57,27 @@ export default function HomePage(_props: HomePageProps): JSX.Element {
         languageAlternates={languageAlternates}
       />
       <PageContent className="text-neutral-100 bg-gradient-to-br from-neutral-800 to-neutral-900">
-        <div className="flex flex-col p-8 mx-auto max-w-80ch">
-          <div className="flex flex-col-reverse py-24 xs:py-48">
-            <h1
-              className="text-5xl font-black tracking-tighter 2xs:text-6xl xs:text-7xl md:text-8xl"
-              style={{ lineHeight: 0.8 }}
-            >
+        <div className="flex flex-col max-w-6xl gap-16 p-8 py-24 mx-auto xs:py-48">
+          <div className="flex flex-col-reverse">
+            <h1 className="text-5xl font-black tracking-tighter 2xs:text-6xl xs:text-7xl md:text-8xl">
               {title}
             </h1>
-            <h2 className="mb-4 text-sm font-semibold tracking-tight uppercase xs:text-lg text-neutral-400">
+
+            {/* <h2 className="mb-4 text-sm font-semibold tracking-tight uppercase xs:text-lg text-neutral-400">
               {subtitle}
-            </h2>
+            </h2> */}
           </div>
-          <div className="py-16">
+
+          <div className="grid flex-1 gap-4 text-2xl font-medium text-neutral-400">
+            <Mdx />
+          </div>
+
+          <div className="">
             <Link href={routes.resources({ kind: 'posts' })}>
               <a className="inline-flex px-12 py-4 text-base font-medium tracking-wide text-white uppercase transition rounded xs:text-lg bg-primary-600 hover:bg-primary-700 focus:outline-none focus-visible:ring focus-visible:ring-primary-600">
                 {t('common.browse')}
               </a>
             </Link>
-          </div>
-          <div className="flex-1">
-            <Mdx />
           </div>
         </div>
       </PageContent>
