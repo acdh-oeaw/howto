be26b0a3afac59514bcf87e552764390aadba69f
Stefan Probst <stefanprobst@posteo.de>
2023-03-21 10:36:11 +0100

    fix: update shiki wasm links for cms preview

--- src/cms/previews/getSyntaxHighlighter.ts
+++ src/cms/previews/getSyntaxHighlighter.ts
@@ -8,8 +8,8 @@ export async function getSyntaxHighlighter(): Promise<Highlighter> {
     /* @ts-expect-error Missing module declaration. */
     'shiki/dist/index.browser.mjs'
   )
-  setOnigasmWASM('https://unpkg.com/shiki@0.9.6/dist/onigasm.wasm')
-  setCDN('https://unpkg.com/shiki@0.9.6/')
+  setOnigasmWASM('https://unpkg.com/shiki@0.14.1/dist/onig.wasm')
+  setCDN('https://unpkg.com/shiki@0.14.1/')
   const highlighter = await getHighlighter({
     theme: 'poimandres',
     langs: supportedLanguages,
