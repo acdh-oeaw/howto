87952aed5f2d2263e8fdf57b0bf01ca84fa4c115
Stefan Probst <stefanprobst@posteo.de>
2023-08-31 19:04:20 +0200

    refactor: use new imprint service [skip ci]

--- config/imprint.config.ts
+++ config/imprint.config.ts
@@ -2,17 +2,12 @@ import { type Locale } from "@/i18n/i18n.config";
 import { createUrl } from "@/utils/createUrl";
 
 /**
- * Creates URL to fetch imprint from ACDH imprint service
- *
- * @see https://fundament.acdh.oeaw.ac.at/imprint-service
+ * Creates URL to fetch imprint from ACDH imprint service.
  */
 export function createImprintUrl(locale: Locale): URL {
 	return createUrl({
-		baseUrl: "https://shared.acdh.oeaw.ac.at",
-		pathname: "/acdh-common-assets/api/imprint.php",
-		query: {
-			serviceID: 19273,
-			outputLang: locale,
-		},
+		baseUrl: "https://imprint.acdh.oeaw.ac.at",
+		pathname: "/19273",
+		query: { locale },
 	});
 }
