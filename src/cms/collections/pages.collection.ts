a1145f498eb608d6bde5f68275fc61d358e49d61
Stefan Probst <stefanprobst@posteo.de>
2023-10-17 12:33:26 +0200

    fix: allow editing landing page title and content via cms

--- src/cms/collections/pages.collection.ts
+++ src/cms/collections/pages.collection.ts
@@ -10,10 +10,14 @@ export const collection: CmsCollection = {
 	description: "",
 	format: "frontmatter",
 	files: [
+		/**
+		 * NetlifyCMS currently only supports "single_file" i18n mode for file collections,
+		 * which we don't want, so we expose two separate file entries.
+		 */
 		{
-			file: "content/pages/home.mdx",
-			name: "home",
-			label: "Home page",
+			file: "content/pages/de/home.mdx",
+			name: "home-de",
+			label: "Home page (german)",
 			fields: [
 				{
 					name: "title",
@@ -21,8 +25,21 @@ export const collection: CmsCollection = {
 					hint: "",
 				},
 				{
-					name: "subtitle",
-					label: "Subtitle",
+					name: "body",
+					label: "Content",
+					hint: "",
+					widget: "markdown",
+				},
+			],
+		},
+		{
+			file: "content/pages/en/home.mdx",
+			name: "home-en",
+			label: "Home page (english)",
+			fields: [
+				{
+					name: "title",
+					label: "Title",
 					hint: "",
 				},
 				{
