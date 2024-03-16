611b63e9ef04f77ecf40903f2c5b25e719548023
Stefan Probst <stefanprobst@posteo.de>
2024-03-16 15:16:08 +0100

    style: adjust h1 line height

--- components/ui/page-title.tsx
+++ components/ui/page-title.tsx
@@ -8,7 +8,7 @@ export function PageTitle(props: PageTitleProps) {
 	const { children } = props;
 
 	return (
-		<h1 className="text-balance font-heading text-3xl font-bold leading-tight tracking-tighter text-neutral-950 xs:text-4xl md:text-5xl dark:text-neutral-0">
+		<h1 className="text-balance font-heading text-3xl font-bold leading-[1.125] tracking-tighter text-neutral-950 xs:text-4xl md:text-5xl dark:text-neutral-0">
 			{children}
 		</h1>
 	);
