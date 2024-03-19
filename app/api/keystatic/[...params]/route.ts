b65d295486991dd6aebda14e316d4b9380a21b9c
Stefan Probst <stefanprobst@posteo.de>
2024-03-19 08:03:41 +0100

    fix: use x-forwarded-host header to rewrite hostname

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,5 +1,34 @@
+import { isNonEmptyString } from "@acdh-oeaw/lib";
 import { makeRouteHandler } from "@keystatic/next/route-handler";
 
 import config from "@/keystatic.config";
 
-export const { GET, POST } = makeRouteHandler({ config });
+const { GET: _GET, POST: _POST } = makeRouteHandler({ config });
+
+/**
+ * @see https://github.com/Thinkmill/keystatic/issues/978#issuecomment-2005730530
+ * @see https://github.com/Thinkmill/keystatic/issues/1022
+ */
+function rewriteUrl(request: Request) {
+	const forwardedHost = request.headers.get("x-forwarded-host");
+	const forwardedProto = request.headers.get("x-forwarded-proto");
+
+	if (isNonEmptyString(forwardedHost) && isNonEmptyString(forwardedProto)) {
+		const url = new URL(request.url);
+
+		url.hostname = forwardedHost;
+		url.protocol = forwardedProto;
+
+		return new Request(url, request);
+	}
+
+	return request;
+}
+
+export function GET(request: Request) {
+	return _GET(rewriteUrl(request));
+}
+
+export function POST(request: Request) {
+	return _POST(rewriteUrl(request));
+}
