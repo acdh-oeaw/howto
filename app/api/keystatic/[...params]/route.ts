cf4376e05787f473afd446f123ec394c811fb9ee
Stefan Probst <stefanprobst@posteo.de>
2024-03-20 10:32:11 +0100

    fix: use forwarded port

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -12,13 +12,14 @@ const { GET: _GET, POST: _POST } = makeRouteHandler({ config });
 function rewriteUrl(request: Request) {
 	const forwardedHost = request.headers.get("x-forwarded-host");
 	const forwardedProto = request.headers.get("x-forwarded-proto");
+	const forwardedPort = request.headers.get("x-forwarded-port");
 
 	if (isNonEmptyString(forwardedHost) && isNonEmptyString(forwardedProto)) {
 		const url = new URL(request.url);
 
 		url.hostname = forwardedHost;
 		url.protocol = forwardedProto;
-		url.port = "";
+		url.port = forwardedPort ?? "";
 
 		return new Request(url, request);
 	}
