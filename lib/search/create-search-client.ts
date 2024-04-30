cb5a4c8aa6620cd1783ac43ebdd6beb80027a73e
Stefan Probst <stefanprobst@posteo.de>
2024-04-30 19:30:18 +0200

    fix: assert valid typesense config

--- lib/search/create-search-client.ts
+++ lib/search/create-search-client.ts
@@ -1,16 +1,20 @@
+import { assert } from "@acdh-oeaw/lib";
 import { Client } from "typesense";
 
 import { env } from "@/config/env.config";
 
 export function createSearchClient(apiKey = env.NEXT_PUBLIC_TYPESENSE_API_KEY) {
+	const host = env.NEXT_PUBLIC_TYPESENSE_HOST;
+	const port = env.NEXT_PUBLIC_TYPESENSE_PORT;
+	const protocol = env.NEXT_PUBLIC_TYPESENSE_PROTOCOL;
+
+	assert(apiKey, "Invalid typesense config.");
+	assert(host, "Invalid typesense config.");
+	assert(port, "Invalid typesense config.");
+	assert(protocol, "Invalid typesense config.");
+
 	const client = new Client({
-		nodes: [
-			{
-				host: env.NEXT_PUBLIC_TYPESENSE_HOST,
-				port: env.NEXT_PUBLIC_TYPESENSE_PORT,
-				protocol: env.NEXT_PUBLIC_TYPESENSE_PROTOCOL,
-			},
-		],
+		nodes: [{ host, port, protocol }],
 		apiKey,
 	});
 
